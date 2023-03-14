ce6d0cd01026a7a474d5e4c8a31ea41a2d84d0b6
Oliver McManus <omcmanus1@gmail.com>
2023-03-14 14:23:13 +0000

    create SingleReview template, add routes, wrap ReviewCard in relevant Link

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./Components/Navbar";
-import Reviews from "./Components/Reviews"
+import Reviews from "./Components/Reviews";
+import SingleReview from "./Components/SingleReview";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Reviews />
+      <Routes>
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/reviews/:review_id" element={<SingleReview />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Components/SingleReview.jsx
@@ -0,0 +1,3 @@
+export default function SingleReview() {
+  return <h2>This Is A Single Review</h2>;
+}
